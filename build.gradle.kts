417ef46056ed31b4bd48440879de765c6fc968ec
Squaredcandy <squaredcandy@gmail.com>
2020-09-21 20:29:22 +1000

    Adding setup code

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "com.squaredcandy"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
